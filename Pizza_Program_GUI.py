54da8640c35920b981a34fb5ff0e40b7a590c224
Maori-boi <micah_pee@student.asdah.school.nz>
2022-04-05 14:17:27 +1200

    Made total order calculation

--- Pizza_Program_GUI.py
+++ Pizza_Program_GUI.py
@@ -1,16 +1,31 @@
 import tkinter as tk
 from tkinter import ttk
 
+def calculate_pizza():
+
+    pizza = int(entry.get()) 
+
+    if(pizza==regular_pizza):
+        print()
+        result_pizza1 = pizza * 8.50
+
+    elif(pizza==gourmet_pizza):
+        print()
+        result_pizza2 = pizza * 13.50
+
+    result_label1.configure(text = "$".format(result_pizza1 + result_pizza2))
+
 root = tk.Tk()
+
+pizza = tk.IntVar()
+pizza.set(1)
 
 variable = tk.StringVar()
 regular_pizza = ["Hawaiian Pizza", "Cheese Pizza", "BBQ Beef and Onion Pizza", "Pepperoni Pizza", "Cheezy Garlic Pizza", "Veggie lover Pizza", "Hot and Spicy Veggie Pizza"]
 gourmet_pizza = ["Garlic Shrimp Pizza", "Chicken Cranberry Pizza", "Buffalo Chicken Pizza", "Mega Meat lovers Pizza", "Meat lovers Hot-dog stuffed Pizza"]
 
-
-#This widget creates a Radiobutton
-radiobutton = ttk.Radiobutton(root, text = "Option 1")
-radiobutton.grid(row = 0, column = 1)
+Heading = ttk.Label(root, text = "Dream Pizza",)
+Heading.grid(row = 0, column = 0, columnspan = 2)
 
 #This widget creates an OptionMenu
 optionmenu = ttk.OptionMenu(root, variable, *regular_pizza + gourmet_pizza)
@@ -21,11 +36,18 @@ entry = ttk.Entry(root)
 entry.grid(row = 1, column = 1)
 
 #This widget creates a text box/label
-label = ttk.Label(root, text = regular_pizza)
-label.grid(row = 2, column = 0, columnspan = 2)
+RegularPizzaMenu = ttk.Label(root, text = regular_pizza)
+RegularPizzaMenu.grid(row = 2, column = 0,)
+
+GourmetPizzaMenu = ttk.Label(root, text = gourmet_pizza)
+GourmetPizzaMenu.grid(row = 2, column = 1,)
 
 #This widget creates a Button
 button = ttk.Button(root, text = "Next",)
 button.grid(row = 5, column = 0, columnspan = 2)
+
+
+result_label1 = ttk.Label(root, text = "$ ")
+result_label1.grid(row = 1, column = 0, columnspan = 3,)
 
 root.mainloop()
