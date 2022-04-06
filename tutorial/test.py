5b1b32c72d9d366556d5500cdb671144fb6ec616
Maori-boi <micah_pee@student.asdah.school.nz>
2022-04-06 12:30:58 +1200

    Added spare code

--- tutorial/test.py
+++ tutorial/test.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Label, ttk
 
 root = tk.Tk()
 
@@ -55,3 +55,38 @@ for Pizza in regular_pizza:
     
     text_box.insert("end", Pizza)
     text_box.insert("end", '\n')
+
+pizza = tk.IntVar()
+pizza.set(1)
+
+def calculate_pizza():
+
+    pizza = int(Label.get()) 
+
+    if(pizza==regular_pizza):
+        print()
+        result_pizza1 = pizza * 8.50
+
+    elif(pizza==gourmet_pizza):
+        print()
+        result_pizza2 = pizza * 13.50
+
+    result_label1.configure(text = "$".format(result_pizza1 + result_pizza2))
+
+    #Prints out Reciept of total order
+result_label1 = ttk.Label(root, text = "$ ")
+result_label1.grid(row = 1, column = 0, columnspan = 3)
+
+#Customer Details if order is for delivery
+Name = ttk.Entry(root)
+Name.grid(row = 0, column = 1)
+
+Address = ttk.Entry(root)
+Address.grid(row = 1, column = 1)
+
+PhoneNumber = ttk.Entry(root)
+PhoneNumber.grid(row = 2, column = 1)
+
+#Customer Details if order is for pick up
+Name = ttk.Entry(root)
+Name.grid(row = 0, column = 1)
