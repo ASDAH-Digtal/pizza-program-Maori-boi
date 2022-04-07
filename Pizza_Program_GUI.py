d0c01dcbcfaa72cf9728d37a4aa31102dfa75a81
Maori-boi <micah_pee@student.asdah.school.nz>
2022-04-07 13:07:46 +1200

    Completed code for page one of GUI

--- Pizza_Program_GUI.py
+++ Pizza_Program_GUI.py
@@ -3,25 +3,22 @@ from tkinter import ttk
 
 root = tk.Tk()
 
+variable = tk.StringVar()
 #Lists for each Pizza Menu
 regularPizza = ["Hawaiian Pizza", "Cheese Pizza", "BBQ Beef and Onion Pizza", "Pepperoni Pizza", "Cheezy Garlic Pizza", "Veggie lover Pizza", "Hot and Spicy Veggie Pizza"]
-gourmetPizza = ["Garlic Shrimp Pizza", "Chicken Cranberry Pizza", "Buffalo Chicken Pizza", "Mega Meat lovers Pizza", "Meat lovers Hot-dog stuffed Pizza"]
-variable = tk.StringVar()
-
-#Combines both lists
-reg_gour_pizza = regularPizza + gourmetPizza
+gourmetPizza = ["Garlic Shrimp Pizza", "Chicken Cranberry Pizza", "Buffalo Chicken Pizza", "Mega Meat lovers Pizza", "Meat lovers Hot-dog stuffed Pizza"] 
 
 #Heading of the GUI
 Heading = ttk.Label(root, text = "Dream Pizza")
 Heading.grid(row = 0, column = 0, columnspan = 2)
 
 #This widget creates a title for regular pizza
-Regular_title = ttk.Label(root, text = "Regular pizza")
-Regular_title.grid(row = 1, column = 0,)
+Regular_title = ttk.Label(root, text = "Regular pizza: $8.50")
+Regular_title.grid(row = 1, column = 0)
 
 #This widget creates a title for gourmet pizza
-Gourmet_title = ttk.Label(root, text = "Gourmet pizza")
-Gourmet_title.grid(row = 1, column = 1,)
+Gourmet_title = ttk.Label(root, text = "Gourmet pizza: $13.50")
+Gourmet_title.grid(row = 1, column = 1)
 
 #This widget creates the regular pizza menu
 regular_menu = ttk.Label(root, text = "Hawaiian Pizza\nCheese Pizza\nBBQ Beef and Onion Pizza\nPepperoni Pizza\nCheezy Garlic Pizza\nVeggie lover Pizza\nHot and Spicy Veggie Pizza")
@@ -29,23 +26,23 @@ regular_menu.grid(row = 2, column = 0)
 
 #This widget creates the gourmet pizza menu
 regular_menu = ttk.Label(root, text = "Garlic Shrimp Pizza\nChicken Cranberry Pizza\nBuffalo Chicken Pizza\nMega Meat lovers Pizza\nMeat lovers Hot-dog stuffed Pizza")
-regular_menu.grid(row = 2, column = 0)
+regular_menu.grid(row = 2, column = 1)
 
 #These widget's creates an OptionMenu for Dream Pizza
-SelectPizza1 = ttk.OptionMenu(root, variable, text = reg_gour_pizza)
-SelectPizza1.grid(row = 3, column = 0)
+optionmenu1 = ttk.OptionMenu(root, variable, *regularPizza + gourmetPizza)
+optionmenu1.grid(row = 3, column = 0)
 
-SelectPizza2 = ttk.OptionMenu(root, variable, text = reg_gour_pizza)
-SelectPizza2.grid(row = 4, column = 0)
+optionmenu2 = ttk.OptionMenu(root, variable, *regularPizza + gourmetPizza)
+optionmenu2.grid(row = 4, column = 0)
 
-SelectPizza3 = ttk.OptionMenu(root, variable, text = reg_gour_pizza)
-SelectPizza3.grid(row = 5, column = 0)
+optionmenu3 = ttk.OptionMenu(root, variable, *regularPizza + gourmetPizza)
+optionmenu3.grid(row = 5, column = 0)
 
-SelectPizza4 = ttk.OptionMenu(root, variable, text = reg_gour_pizza)
-SelectPizza4.grid(row = 6, column = 0)
+optionmenu4 = ttk.OptionMenu(root, variable, *regularPizza + gourmetPizza)
+optionmenu4.grid(row = 6, column = 0)
 
-SelectPizza5 = ttk.OptionMenu(root, variable, text = reg_gour_pizza)
-SelectPizza5.grid(row = 7, column = 0)
+optionmenu5 = ttk.OptionMenu(root, variable, *regularPizza + gourmetPizza)
+optionmenu5.grid(row = 7, column = 0)
 
 #These widgets show the price of the pizza
 price_pizza = ttk.Label(root, text = "$0.00")
