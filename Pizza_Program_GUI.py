4a7cab2ad6c7b4c1a927d81c5af9f28d6f836a04
Maori-boi <micah_pee@student.asdah.school.nz>
2022-04-07 13:30:37 +1200

    Made changes to the font and the padding

--- Pizza_Program_GUI.py
+++ Pizza_Program_GUI.py
@@ -1,31 +1,32 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.font import BOLD
 
 root = tk.Tk()
 
 variable = tk.StringVar()
 #Lists for each Pizza Menu
-regularPizza = ["Hawaiian Pizza", "Cheese Pizza", "BBQ Beef and Onion Pizza", "Pepperoni Pizza", "Cheezy Garlic Pizza", "Veggie lover Pizza", "Hot and Spicy Veggie Pizza"]
-gourmetPizza = ["Garlic Shrimp Pizza", "Chicken Cranberry Pizza", "Buffalo Chicken Pizza", "Mega Meat lovers Pizza", "Meat lovers Hot-dog stuffed Pizza"] 
+regularPizza = ["Select Pizza","Hawaiian Pizza", "Cheese Pizza", "BBQ Beef and Onion Pizza", "Pepperoni Pizza", "Cheezy Garlic Pizza", "Veggie lover Pizza", "Hot and Spicy Veggie Pizza"]
+gourmetPizza = ["Select Pizza","Garlic Shrimp Pizza", "Chicken Cranberry Pizza", "Buffalo Chicken Pizza", "Mega Meat lovers Pizza", "Meat lovers Hot-dog stuffed Pizza"] 
 
 #Heading of the GUI
-Heading = ttk.Label(root, text = "Dream Pizza")
+Heading = ttk.Label(root, text = "Dream Pizza", padding = 15, font = 10)
 Heading.grid(row = 0, column = 0, columnspan = 2)
 
 #This widget creates a title for regular pizza
-Regular_title = ttk.Label(root, text = "Regular pizza: $8.50")
+Regular_title = ttk.Label(root, text = "Regular pizza: $8.50", padding = 5, font = 5)
 Regular_title.grid(row = 1, column = 0)
 
 #This widget creates a title for gourmet pizza
-Gourmet_title = ttk.Label(root, text = "Gourmet pizza: $13.50")
+Gourmet_title = ttk.Label(root, text = "Gourmet pizza: $13.50", padding = 5, font = 5)
 Gourmet_title.grid(row = 1, column = 1)
 
 #This widget creates the regular pizza menu
-regular_menu = ttk.Label(root, text = "Hawaiian Pizza\nCheese Pizza\nBBQ Beef and Onion Pizza\nPepperoni Pizza\nCheezy Garlic Pizza\nVeggie lover Pizza\nHot and Spicy Veggie Pizza")
+regular_menu = ttk.Label(root, text = "Hawaiian Pizza\nCheese Pizza\nBBQ Beef and Onion Pizza\nPepperoni Pizza\nCheezy Garlic Pizza\nVeggie lover Pizza\nHot and Spicy Veggie Pizza", padding = 10)
 regular_menu.grid(row = 2, column = 0)
 
 #This widget creates the gourmet pizza menu
-regular_menu = ttk.Label(root, text = "Garlic Shrimp Pizza\nChicken Cranberry Pizza\nBuffalo Chicken Pizza\nMega Meat lovers Pizza\nMeat lovers Hot-dog stuffed Pizza")
+regular_menu = ttk.Label(root, text = "Garlic Shrimp Pizza\nChicken Cranberry Pizza\nBuffalo Chicken Pizza\nMega Meat lovers Pizza\nMeat lovers Hot-dog stuffed Pizza", padding = 10)
 regular_menu.grid(row = 2, column = 1)
 
 #These widget's creates an OptionMenu for Dream Pizza
